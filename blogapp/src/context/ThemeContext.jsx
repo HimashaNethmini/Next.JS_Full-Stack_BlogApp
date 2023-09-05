61d3e51139828da94a79ed95915edf9d08bc9690
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-05 18:35:35 +0530

    if its the client component, use local storage

--- blogapp/src/context/ThemeContext.jsx
+++ blogapp/src/context/ThemeContext.jsx
@@ -1,4 +1,34 @@
-import { createContext } from "react";
+"use client";
 
-//having the dark mode functionality
-export default ThemeContext = createContext
+import { createContext, useEffect, useState } from "react";
+
+export const ThemeContext = createContext();
+
+//if its the client component, use local storage
+const getFromLocalStorage = () => {
+  if (typeof window !== "undefined") {
+    const value = localStorage.getItem("theme");
+    return value || "light";
+  }
+};
+
+export const ThemeContextProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    return getFromLocalStorage();
+  });
+
+  const toggle = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  //set the status in the local storage
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggle }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};

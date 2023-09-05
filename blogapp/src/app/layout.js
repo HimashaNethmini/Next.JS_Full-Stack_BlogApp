aa7674c731e46c7a42ba03e39f2f1cffa927cab3
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-05 21:24:05 +0530

    wrap using authprovider

--- blogapp/src/app/layout.js
+++ blogapp/src/app/layout.js
@@ -4,6 +4,7 @@ import Navbar from '@/component/navbar/Navbar';
 import Footer from '@/component/footer/Footer';
 import { ThemeContextProvider } from '@/context/ThemeContext';
 import ThemeProvider from '@/providers/ThemeProvider';
+import AuthProvider from '@/providers/AuthProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/*<AuthProvider>*/}
+        <AuthProvider>
           <ThemeContextProvider>
             <ThemeProvider>
           <div className='container'>
@@ -28,8 +29,7 @@ export default function RootLayout({ children }) {
         </div>
         </ThemeProvider>
           </ThemeContextProvider>
-        
-        {/*</AuthProvider>*/}
+        </AuthProvider>
       </body>
     </html>
   )

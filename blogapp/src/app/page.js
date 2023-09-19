858879abd0dd9e692c4390bc4055074083e72868
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-19 20:23:32 +0530

    added all components to render

--- blogapp/src/app/page.js
+++ blogapp/src/app/page.js
@@ -1,13 +1,21 @@
 import React from "react";
+import styles from './homepage.module.css'
 import '../component/cardList/cardList.module.css';
-import Pagination from "../component/pagination/Pagination"
 import Featured from "@/component/featured/Featured";
+import CategoryList from "@/component/categoryList/CategoryList";
+import CardList from "@/component/cardList/CardList";
+import Menu from "@/component/Menu/Menu";
 
 export default function Home() {
   return (
     <div className="container">
       <Featured />
-      <Pagination />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+     
 
     </div>
   )
